b052a7ce0cf694e652a094d1c3f538f0b445d2bd
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-05-02 19:16:51 +0600

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const chef = require('./data/chefs.json');
+
+app.use(cors());
+
+
 
 app.get('/' , (req, res) => {
     res.send('Server is running')
 
+})
+
+app.get('/main' , (req, res) => {
+    res.send(chef)
 })
 
 app.listen(port, ()=> {
