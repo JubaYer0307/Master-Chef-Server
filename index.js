186738b518bd362e379d52de1fa23cd178cd27e3
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-05-03 11:28:31 +0600

    recipies data added

--- index.js
+++ index.js
@@ -3,21 +3,47 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const chef = require('./data/chefs.json');
+const chefs = require('./data/chefs.json');
+
+const recipies = require('./data/recipies.json');
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.get('/main', (req, res) => {
+  res.send(chefs);
+});
+
+app.get('/recipies', (req, res) => {
+  res.send(recipies);
+});
+
+app.get('/main/:id', (req, res) => {
+  const chefId = req.params.id;
+  const chef = chefs.find((chef) => chef.id === chefId);
+
+  if (chef) {
+    res.send(chef);
+  } else {
+    res.status(404).send('Chef not found');
+  }
+});
+
+app.get('/recipies/:id', (req, res) => {
+  const recipieId = req.params.id;
+  const recipie = chefs.find((recipie) => recipie.id === recipiefId);
+
+  if (recipie) {
+    res.send(recipie);
+  } else {
+    res.status(404).send('recipie not found');
+  }
+});
 
 
-app.get('/' , (req, res) => {
-    res.send('Server is running')
-
-})
-
-app.get('/main' , (req, res) => {
-    res.send(chef)
-})
-
-app.listen(port, ()=> {
-    console.log(`server is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
